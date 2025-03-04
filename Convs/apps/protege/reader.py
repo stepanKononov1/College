5f4a50800855fabb440c2cdce312d9faee3a1514
Stepasha <stepan.kot.67@mail.ru>
2025-03-04 19:18:26 +0300

    task 3 update

--- Convs/apps/protege/reader.py
+++ Convs/apps/protege/reader.py
@@ -2,20 +2,22 @@ from owlready2 import *
 
 onto = get_ontology("file://myowl.owl").load()
 
-def recommend_gadget():
+
+def recomend():
     actor = input("Предпочитаемый актёр (Степан/Мариарти): ") or None
     genre = input("Предпочитаемый жанр (Комедия/Ужасы): ") or None
-    director = input("Предпочитаемый режиссёр (Джо_Байден/Френк_Синатра): ") or None
-    
+    director = input(
+        "Предпочитаемый режиссёр (Джо_Байден/Френк_Синатра): ") or None
+
     recommended_film = None
     for film in onto.Фильм.instances():
         matches = True
         if actor:
-            actors = [str(b.name) for b in film.имеетАктёра] 
+            actors = [str(b.name) for b in film.имеетАктёра]
             if actor not in actors:
                 matches = False
         if genre:
-            genres = [str(t.name) for t in film.имеетЖанр] 
+            genres = [str(t.name) for t in film.имеетЖанр]
             if genre not in genres:
                 matches = False
         if director:
@@ -24,13 +26,14 @@ def recommend_gadget():
                 matches = False
         if matches:
             recommended_film = film.name
-            break  
-    
+            break
 
     if recommended_film:
-        print(f"\nНа основе ваших предпочтений я рекомендую вам фильм: {recommended_film}")
+        print(
+            f"\nНа основе ваших предпочтений я рекомендую вам фильм: {recommended_film}")
     else:
         print("\nТаких фильмов нет")
 
+
 if __name__ == "__main__":
-    recommend_gadget()
+    recomend()
